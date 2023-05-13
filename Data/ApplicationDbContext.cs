3e86ef3ccbce151fee6b0445d71d2785ebd4f4a0
delish-khadka <delishkhadka@gmail.com>
2023-05-13 15:54:04 +0545

    Added many to many relation in DbContext and added class for seeding

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -20,6 +20,27 @@ namespace PokemonReviewApp.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<PokemonCategory>()
+                .HasKey(pc => new {pc.PokemonId, pc.CategoryId});
+            modelBuilder.Entity<PokemonCategory>()
+                .HasOne(p => p.Pokemon)
+                .WithMany(pc => pc.PokemonCategories)
+                .HasForeignKey(p => p.PokemonId);
+            modelBuilder.Entity<PokemonCategory>()
+                .HasOne(p => p.Category)
+                .WithMany(pc => pc.PokemonCategories)
+                .HasForeignKey(p => p.CategoryId);
+
+            modelBuilder.Entity<PokemonOwner>()
+                .HasKey(po => new { po.PokemonId, po.OwnerId });
+            modelBuilder.Entity<PokemonOwner>()
+                .HasOne(p => p.Pokemon)
+                .WithMany(pc => pc.PokemonOwners)
+                .HasForeignKey(p => p.PokemonId);
+            modelBuilder.Entity<PokemonOwner>()
+                .HasOne(p => p.Owner)
+                .WithMany(pc => pc.PokemonOwners)
+                .HasForeignKey(p => p.OwnerId);
 
         }
     }
--- Models/Category.cs
+++ Models/Category.cs
@@ -2,7 +2,7 @@
 {
     public class Category
     {
-        public int PokemonId { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public ICollection<PokemonCategory> PokemonCategories { get; set; }
         

e994716c2d904fbb0e69585257493595add5dcb9
delish-khadka <delishkhadka@gmail.com>
2024-06-18 23:14:57 +0545

    Made all of the owner Operations asynchronous

--- Interfaces/IOwnerRepository.cs
+++ Interfaces/IOwnerRepository.cs
@@ -4,14 +4,14 @@ namespace PokemonReviewApp.Interfaces
 {
     public interface IOwnerRepository
     {
-        ICollection<Owner> GetOwners();
-        Owner GetOwner(int ownerId);
-        ICollection<Owner> GetOwnerOfAPokemon(int pokeId);
-        ICollection<Pokemon> GetPokemonByOwner(int ownerId);
-        bool OwnerExists(int ownerId);
-        bool CreateOwner(Owner owner);
-        bool DeleteOwner(Owner owner);
-        bool Save();
-        bool UpdateOwner(Owner owner);
+        Task<ICollection<Owner>> GetOwners();
+        Task<Owner> GetOwner(int ownerId);
+        Task<ICollection<Owner>> GetOwnerOfAPokemon(int pokeId);
+        Task<ICollection<Pokemon>> GetPokemonByOwner(int ownerId);
+        Task<bool> OwnerExists(int ownerId);
+        Task<bool> CreateOwner(Owner owner);
+        Task<bool> DeleteOwner(Owner owner);
+        Task<bool> Save();
+        Task<bool> UpdateOwner(Owner owner);
     }
 }
